09201104ff04593aa06afad762441767a05c5dd5
Chivas-YJC <1600017714@pku.edu.cn>
2018-12-26 22:26:25 -0800

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chivas/Desktop/spider/src
+# Build directory: /home/chivas/Desktop/spider/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(spider_description)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/chivas/Desktop/spider/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/chivas/Desktop/spider/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/chivas/Desktop/spider/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/chivas/Desktop/spider/src:$ROS_PACKAGE_PATH"
